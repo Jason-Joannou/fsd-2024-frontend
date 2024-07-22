ca34b2876ac7a2984a67a997300e0e0e55bb1e40
Jason Joannou <jjoannou@bscglobal.com>
2024-07-22 14:23:14 +0200

    Adding logic slowly to move api calls into dashboard component

--- src/components/FinancialDashboard.vue
+++ src/components/FinancialDashboard.vue
@@ -16,7 +16,9 @@
       <!-- Aside -->
       <el-aside width="200px" class="aside">
         <el-menu :default-active="currentView" class="el-menu-vertical-demo" @select="handleMenuSelect">
+          <el-menu-item index="welcome"><i class="el-icon-menu"></i>Home</el-menu-item>
           <el-menu-item index="dashboard"><i class="el-icon-menu"></i>Dashboard</el-menu-item>
+          <el-menu-item index="data-info"><i class="el-icon-menu"></i>Data Information</el-menu-item>
           <el-menu-item index="machine-learning"><i class="el-icon-menu"></i>Machine Learning</el-menu-item>
           <el-menu-item index="settings"><i class="el-icon-setting"></i>Settings</el-menu-item>
         </el-menu>
@@ -24,19 +26,38 @@
 
       <!-- Main content -->
       <el-main class="main-content">
-        <div v-if="currentView === 'dashboard'">
+        <div v-if="currentView === 'welcome'">
+          <HomePage/>
+        </div>
+        <div v-else-if="currentView === 'dashboard'">
           <el-card class="box-card">
             <div>Welcome to your financial dashboard.</div>
           </el-card>
           <el-card class="box-card" style="margin-top: 20px;">
             <template v-slot:header>
-              <span>Chart</span>
+              <div class="chart-header">
+                <span>Chart selection</span>
+                <el-select v-model="selectedGraph" placeholder="Select a graph" @change="onGraphSelect" class="graph-select">
+                  <el-option v-for="option in graphOptions" :key="option.value" :label="option.label" :value="option.value">
+                  </el-option>
+                </el-select>
+              </div>
             </template>
-            <el-table :data="tableData" border style="width: 100%">
-              <el-table-column prop="date" label="Date" width="180"></el-table-column>
-              <el-table-column prop="name" label="Name" width="180"></el-table-column>
-              <el-table-column prop="address" label="Address"></el-table-column>
-            </el-table>
+            <div v-if="selectedGraph === 'table'">
+              <el-table :data="tableData" border style="width: 100%">
+                <el-table-column prop="date" label="Date" width="180"></el-table-column>
+                <el-table-column prop="name" label="Name" width="180"></el-table-column>
+                <el-table-column prop="address" label="Address"></el-table-column>
+              </el-table>
+            </div>
+            <div v-if="selectedGraph === 'line'">
+              <!-- Line chart implementation goes here -->
+              <p>Line chart will be displayed here.</p>
+            </div>
+            <div v-if="selectedGraph === 'bar'">
+              <!-- Bar chart implementation goes here -->
+              <p>Bar chart will be displayed here.</p>
+            </div>
           </el-card>
         </div>
         <div v-else-if="currentView === 'machine-learning'">
@@ -59,6 +80,7 @@
 
 <script>
 import SettingsPage from './SettingsPage.vue';
+import HomePage from './HomePage.vue';
 
 export default {
   name: 'FinancialDashboard',
@@ -66,6 +88,12 @@ export default {
     return {
       username: localStorage.getItem('username') || '', // Retrieve stored username
       currentView: 'dashboard', // Default view
+      selectedGraph: 'table', // Default graph type
+      graphOptions: [
+        { label: 'Table', value: 'table' },
+        { label: 'Line Chart', value: 'line' },
+        { label: 'Bar Chart', value: 'bar' },
+      ],
       tableData: [
         { date: '2016-05-02', name: 'Tom', address: 'New York No. 1 Lake Park' },
         { date: '2016-05-04', name: 'Tom', address: 'New York No. 1 Lake Park' },
@@ -81,10 +109,14 @@ export default {
     },
     handleMenuSelect(index) {
       this.currentView = index;
+    },
+    onGraphSelect(value) {
+      this.selectedGraph = value;
     }
   },
   components: {
     SettingsPage,
+    HomePage
   },
 };
 </script>
@@ -121,6 +153,15 @@ export default {
 .signout-button,
 .settings-button {
   margin-left: 12px;
+}
+
+.graph-select {
+  margin-left: 12px;
+}
+
+.chart-header {
+  display: flex;
+  align-items: center;
 }
 
 .aside {
